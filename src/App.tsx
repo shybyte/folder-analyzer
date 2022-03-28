12cf8f1c547829b29bc981472c1ac40c79bfaf2f
shybyte <shybyte@users.noreply.github.com>
2022-03-28 07:23:54 +0200

    First version of FolderPicker.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
+import {onMount} from 'solid-js';
 import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
 import styles from './App.module.css';
+import {FileSystemNode, FolderPicker} from './components/common/FolderPicker';
 
 const App: Component = () => {
+  function onFolderPicked(folder: FileSystemNode) {
+    console.log('folder', folder);
+  }
+
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <FolderPicker onFolderPicked={onFolderPicked}></FolderPicker>
     </div>
   );
 };
---
+++ src/components/common/FolderPicker.tsx
@@ -0,0 +1,102 @@
+import {onMount} from 'solid-js';
+
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/wicg-file-system-access/index.d.ts
+declare global {
+  interface Window {
+    showDirectoryPicker: () => Promise<FileSystemDirectoryHandle>
+  }
+
+  interface FileSystemDirectoryHandle extends FileSystemHandle {
+    readonly kind: 'directory';
+    getDirectoryHandle(name: string, options?: FileSystemGetDirectoryOptions): Promise<FileSystemDirectoryHandle>;
+    getFileHandle(name: string, options?: FileSystemGetFileOptions): Promise<FileSystemFileHandle>;
+    removeEntry(name: string, options?: FileSystemRemoveOptions): Promise<void>;
+    resolve(possibleDescendant: FileSystemHandle): Promise<string[] | null>;
+    keys(): AsyncIterableIterator<string>;
+    values(): AsyncIterableIterator<FileSystemDirectoryHandle | FileSystemFileHandle>;
+    entries(): AsyncIterableIterator<[string, FileSystemDirectoryHandle | FileSystemFileHandle]>;
+    [Symbol.asyncIterator]: FileSystemDirectoryHandle['entries'];
+    readonly isFile: false;
+  }
+}
+
+
+export interface FileSystemNode {
+  name: string;
+  children?: FileSystemNode[];
+}
+
+interface FolderPickerProps {
+  onFolderPicked(fileSystemNodes: FileSystemNode): void;
+}
+
+export const FolderPicker = (props: FolderPickerProps) => {
+  if (!window.showDirectoryPicker) {
+    return FolderPickerOld(props);
+  } else {
+    return FolderPickerNew(props)
+  }
+};
+
+export const FolderPickerNew = (props: FolderPickerProps) => {
+  async function show() {
+    const dirHandle = await window.showDirectoryPicker();
+    const tree = await readFolder(dirHandle)
+    props.onFolderPicked(tree);
+  }
+
+  return (
+    <div>
+      <button onClick={show}>Select Folder</button>
+    </div>
+  );
+};
+
+
+async function readFolder(fileSystemDirectoryHandle: FileSystemDirectoryHandle): Promise<FileSystemNode> {
+  const children = [];
+
+  const result: FileSystemNode = {
+    name: fileSystemDirectoryHandle.name,
+    children: []
+  }
+
+  for await (const entry of fileSystemDirectoryHandle.values()) {
+    if (entry.kind === 'directory') {
+      const child = await readFolder(entry);
+      children.push(child);
+    } else {
+      children.push({
+        name: entry.name
+      });
+    }
+  }
+
+
+  return {
+    children: children,
+    name: fileSystemDirectoryHandle.name
+  };
+}
+
+
+// TODO: Implement FolderPickerProps.onFolderPicked
+export const FolderPickerOld = (_props: FolderPickerProps) => {
+  let fileInput!: HTMLInputElement;
+
+  onMount(() => {
+    fileInput.webkitdirectory = true;
+  })
+
+  function show() {
+    console.log('myInput', fileInput.files);
+  }
+
+  return (
+    <div>
+      <label for="avatar">Choose a folder:</label>
+      <input type="file" ref={fileInput} multiple={true}/>
+      <button onClick={show}>Show</button>
+    </div>
+  );
+};
