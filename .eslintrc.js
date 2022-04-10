edcfe31349cbc848f4c47d8fcf0c04a8040387a1
shybyte <shybyte@users.noreply.github.com>
2022-04-11 00:18:49 +0200

    More linting: eslint-plugin-sonarjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,14 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-  plugins: ['@typescript-eslint', 'eslint-plugin-import'],
+  plugins: ['@typescript-eslint', 'eslint-plugin-import', 'sonarjs'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:import/typescript',
+    'plugin:sonarjs/recommended',
   ],
   rules: {
     'import/no-cycle': 'error',
